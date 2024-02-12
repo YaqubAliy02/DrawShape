5b770e50d4d20dced3ddca8a17a08a524e948da3
Yaqubaliy02 <yaqubaliy02@gmail.com>
2024-02-12 21:03:37 +0500

    the length of circle is calculated

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DrawShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You can calculate length of the circle");
+            Console.Write("Enter radius of circle:");
+            string userStrInput = Console.ReadLine();
+            int R = Convert.ToInt32(userStrInput);
+            const float PI = 3.14f;
+            float result = 2 * PI * R;
+            Console.WriteLine($"The length of circle is {result}sm");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DrawShape
-You can calculate the length of Circle 
+
+You can calculate the length of Circle.
+1) Picture of Circule 
+![image](./assets/images.png)
+
+1) Code => 
+
+![image](./assets/DrawShape.png)
+
+2) Demo =>
+
+
+![image](./assets/demo2.png)
